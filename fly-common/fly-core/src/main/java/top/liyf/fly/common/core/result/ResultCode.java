dc792f42251f51943c7ad892897b9dd310fc1877
93814690 <93814690@qq.com>
2021-06-25 16:09:46 +0800

    add: ResultCode

--- fly-common/fly-core/src/main/java/top/liyf/fly/common/core/result/ResultCode.java
+++ fly-common/fly-core/src/main/java/top/liyf/fly/common/core/result/ResultCode.java
@@ -18,7 +18,15 @@ public enum ResultCode {
 
     CLIENT_ERROR("A0001", "用户端错误"),
 
+    LOGIN_ERROR("A0200", "用户登录异常 "),
+
+    BAD_CREDENTIALS("A0201", "用户名或者密码输入错误 "),
+
+    ACCESS_ERROR("A0300", "访问权限异常 "),
+
     NOT_ALLOWED("A0301", "没有权限"),
+
+    UNAUTHORIZED_ACCESS("A0302", "访问未授权"),
 
     NOT_FOUND("A0404", "未找到"),
 
